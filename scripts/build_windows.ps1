492b5306190f503b69de6c713728db85b40f3896
Kunal Mavani <kunalmavani01@gmail.com>
2026-06-15 20:05:35 +0530

    Use build-only requirements for Windows packaging

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path dist | Out-Null
 python -m pip install -r requirements.txt
-python -m pip install pyinstaller
+python -m pip install -r requirements-desktop.txt
 pyinstaller --noconfirm prompter.spec
